003b5fc4073f192c7a287ea10b9cef5f085c60b6
Delta <ti22mau@gmail.com>
2023-05-23 18:01:58 -0300

    added JS an 'podcast'

---
+++ src/js/podcast-script.js
@@ -0,0 +1,108 @@
+let containerVideoPrin = document.querySelector('.videosContainer')
+let containerVideoSec = document.querySelector('.cvContainer')
+
+containerVideoPrin.innerHTML = `<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/jW6FBfH3CxA"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">A CRIMINALIDADE NO BRASIL TEM SOLUÇÃO</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/ySS03ez17lA"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">A CRIMINALIDADE NO BRASIL E A VIOLÊNCIA NO RIO DE JANEIRO</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/YM9Hzph7c8o"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">AUMENTO DA VIOLÊNCIA NO BRASIL</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/acVxbRAim0g"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">A POBREZA É A CULPADA PELA VIOLÊNCIA NO BRASIL </h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/H1rpoWMYMZs"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">O CRIME COMPENSA NO BRASIL? #SHORTS</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/E_Jqh7BbaPE"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">DIFERENÇA DO PCC X CV - ROGÉRIO GREGO</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/fpEUkJrGIxs"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">CIRO FALA SOBRE CRIME NO RIO/ ALL OVER PODCAST</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/lJ0y4G4X3MY"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">RICARDI SALADA (PERITO CRIMINAL) PODCAST 586</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/WifGANLN7x0"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">MUCALOL FALA SOBRE O CRIME NAS FAVELAS DO RIO DE JANEIRO</h2>
+</div>
+
+<div class="videoPrinc">
+<iframe width="300" height="160" src="https://www.youtube.com/embed/TXDWGR-BoQc"
+    title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen></iframe>
+<h2 class="tituloVideoPrin">MUCALOL - O CRIME NO RIO DE JANEIRO E FILIPE RET </h2>
+</div>`
+
+containerVideoSec.innerHTML = `
+<div class="videoSec">
+    <iframe width="500" height="280" src="https://www.youtube.com/embed/JmYUNK-GBH8"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    <h2 class="tituloVideoSec">Banalização da violência no Brasil: Como estamos nos acostumando com o
+        inaceitável?</h2>
+</div>
+
+<div class="videoSec">
+    <iframe width="500" height="280" src="https://www.youtube.com/embed/sPZMnq0d3pc"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    <h2 class="tituloVideoSec">As origens da violência no Rio de Janeiro e no Brasil, por José Beltrame</h2>
+</div>
+
+<div class="videoSec">
+    <iframe width="500" height="280" src="https://www.youtube.com/embed/Gj2odAHhPA4"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    <h2 class="tituloVideoSec">A violência no Brasil explicada por Sergio Adorno | Entrevista Completa</h2>
+</div>`
